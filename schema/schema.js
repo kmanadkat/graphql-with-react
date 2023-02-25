1869fa85dc053873de8451dbdfca9709ffd2793a
kmanadkat <kmanadkat@gmail.com>
2023-02-25 16:24:22 +0530

    Section 3 - E11 - Root Queries

--- schema/schema.js
+++ schema/schema.js
@@ -15,3 +15,17 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt },
   }
 })
+
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      }
+    }
+  }
+})
