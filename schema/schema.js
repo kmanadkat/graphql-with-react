c9d01d16a0a5f35ff63fcf7492fadd5f18316c4a
kmanadkat <kmanadkat@gmail.com>
2023-02-25 16:38:05 +0530

    Section 3 - E12 - Resolving With Data

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,13 @@
 const graphql = require('graphql')
+const _ = require('lodash')
+
+const users = require('../mockData/users')
 
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql
 
 
@@ -24,8 +28,12 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-
+        return _.find(users, { id: args.id })
       }
     }
   }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
