1fd3763b076f00758ee043128edf8053f74dfabf
kmanadkat <kmanadkat@gmail.com>
2023-02-25 17:45:27 +0530

    Section 3 - E15 - Async Resolve Function

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const _ = require('lodash')
+const axios = require('axios')
 
 const {
   GraphQLObjectType,
@@ -26,7 +26,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        // return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data)
       }
     }
   }
