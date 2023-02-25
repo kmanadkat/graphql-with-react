0d5ddc34308f6f7a589719d77fb5e6c59762d809
kmanadkat <kmanadkat@gmail.com>
2023-02-25 23:05:15 +0530

    Section 4 - E21 - Multiple RootQuery Entry Points

--- schema/schema.js
+++ schema/schema.js
@@ -42,6 +42,13 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data)
       }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`).then(resp => resp.data)
+      }
     }
   }
 })
