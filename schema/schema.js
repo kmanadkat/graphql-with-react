43cc732c1851f8113e8ecc460ad1fe16a71f8273
kmanadkat <kmanadkat@gmail.com>
2023-02-25 16:17:16 +0530

    Section 3 - E10 - Writing GraphQL Schema

---
+++ schema/schema.js
@@ -0,0 +1,17 @@
+const graphql = require('graphql')
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} = graphql
+
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }
+})
