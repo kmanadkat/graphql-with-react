a2909f967af84356577dc16165cdeee2efbb61fc
kmanadkat <kmanadkat@gmail.com>
2023-03-16 22:33:46 +0530

    Fix - Added User Id in UserType Schema

--- auth-graphql/server/schema/types/user_type.js
+++ auth-graphql/server/schema/types/user_type.js
@@ -2,11 +2,13 @@ const graphql = require('graphql')
 const {
   GraphQLObjectType,
   GraphQLString,
+  GraphQLID
 } = graphql
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
+    id: { type: GraphQLID },
     email: { type: GraphQLString }
   })
 })
